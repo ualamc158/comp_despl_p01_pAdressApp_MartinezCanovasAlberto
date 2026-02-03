9ee7c93a3377438ea34c9661ec8e319d1422ba4c
Alberto <albertomartinezcanovas158@gmail.com>
2026-02-03 09:17:52 +0100

    Commit inicial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module es.damdi.alberto.comp_despl_p01_padressapp_martinezcanovasalberto {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens es.damdi.alberto.comp_despl_p01_padressapp_martinezcanovasalberto.model;
+    opens es.damdi.alberto.comp_despl_p01_padressapp_martinezcanovasalberto.persistence;
+    opens es.damdi.alberto.comp_despl_p01_padressapp_martinezcanovasalberto.view;
+
+    opens es.damdi.alberto.comp_despl_p01_padressapp_martinezcanovasalberto to javafx.fxml;
+    exports es.damdi.alberto.comp_despl_p01_padressapp_martinezcanovasalberto;
+}
