6a68bcafacb84126deb75f3977bb68b7e0c403fe
Alberto <albertomartinezcanovas158@gmail.com>
2026-02-05 17:31:32 +0100

    Commit inicial

---
+++ src/main/java/es/damdi/alberto/comp_despl_p01_padressapp_martinezcanovasalberto/view/PersonOverviewController.java
@@ -0,0 +1,126 @@
+package es.damdi.alberto.comp_despl_p01_padressapp_martinezcanovasalberto.view;
+
+import es.damdi.alberto.comp_despl_p01_padressapp_martinezcanovasalberto.MainApp;
+import es.damdi.alberto.comp_despl_p01_padressapp_martinezcanovasalberto.model.Person;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+
+public class PersonOverviewController {
+    @FXML
+    private TableView<Person> personTable;
+    @FXML
+    private TableColumn<Person, String> firstNameColumn;
+    @FXML
+    private TableColumn<Person, String> lastNameColumn;
+
+    @FXML
+    private Label firstNameLabel;
+    @FXML
+    private Label lastNameLabel;
+    @FXML
+    private Label streetLabel;
+    @FXML
+    private Label postalCodeLabel;
+    @FXML
+    private Label cityLabel;
+    @FXML
+    private Label birthdayLabel;
+    @FXML
+    private Button deleteButton;
+    @FXML
+    private Button editButton;
+
+    // Reference to the main application.
+    private MainApp mainApp;
+
+    /**
+     * The constructor.
+     * The constructor is called before the initialize() method.
+     */
+    public PersonOverviewController() {
+    }
+
+    /**
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+    @FXML
+    private void initialize() {
+        // Initialize the person table with the two columns.
+        firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
+        lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+        // Clear person details.
+        showPersonDetails(null);
+
+        // Listen for selection changes and show the person details when changed.
+        personTable.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> showPersonDetails(newValue));
+
+        deleteButton.disableProperty().bind(personTable.getSelectionModel().selectedItemProperty().isNull());
+        editButton.disableProperty().bind(personTable.getSelectionModel().selectedItemProperty().isNull());
+    }
+
+    /**
+     * Is called by the main application to give a reference back to itself.
+     *
+     * @param mainApp
+     */
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+
+        // Add observable list data to the table
+        personTable.setItems(mainApp.getPersonData());
+    }
+
+    /**
+     * Fills all text fields to show details about the person.
+     * If the specified person is null, all text fields are cleared.
+     *
+     * @param person the person or null
+     */
+    private void showPersonDetails(Person person) {
+        if (person != null) {
+            // Fill the labels with info from the person object.
+            firstNameLabel.setText(person.getFirstName());
+            lastNameLabel.setText(person.getLastName());
+            streetLabel.setText(person.getStreet());
+            postalCodeLabel.setText(Integer.toString(person.getPostalCode()));
+            cityLabel.setText(person.getCity());
+
+            // TODO: We need a way to convert the birthday into a String!
+            // birthdayLabel.setText(...);
+        } else {
+            // Person is null, remove all the text.
+            firstNameLabel.setText("");
+            lastNameLabel.setText("");
+            streetLabel.setText("");
+            postalCodeLabel.setText("");
+            cityLabel.setText("");
+            birthdayLabel.setText("");
+        }
+    }
+
+    /**
+     * Called when the user clicks on the delete button.
+     */
+    @FXML
+    private void handleDeletePerson() {
+        int selectedIndex = personTable.getSelectionModel().getSelectedIndex();
+        if (selectedIndex < 0) {
+            return;
+        }
+
+        Person selectedPerson = personTable.getSelectionModel().getSelectedItem();
+            // Nothing selected.
+            Alert confirm = new Alert(Alert.AlertType.CONFIRMATION);
+            confirm.initOwner(mainApp.getPrimaryStage());
+            confirm.setTitle("Confirm Deletion");
+            confirm.setHeaderText("Delete Selected Person");
+            confirm.setContentText("Are you sure you want to delete:\n\n" + selectedPerson.getFirstName() + " " + selectedPerson.getLastName() + "?");
+
+            if(confirm.showAndWait().orElse(ButtonType.CANCEL) == ButtonType.OK){
+                personTable.getItems().remove(selectedIndex);
+        }
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,8 +5,9 @@ module es.damdi.alberto.comp_despl_p01_padressapp_martinezcanovasalberto {
     requires org.kordamp.bootstrapfx.core;
 
     opens es.damdi.alberto.comp_despl_p01_padressapp_martinezcanovasalberto.model;
-    opens es.damdi.alberto.comp_despl_p01_padressapp_martinezcanovasalberto.persistence;
+//    opens es.damdi.alberto.comp_despl_p01_padressapp_martinezcanovasalberto.persistence;
     opens es.damdi.alberto.comp_despl_p01_padressapp_martinezcanovasalberto.view;
+    opens es.damdi.alberto.comp_despl_p01_padressapp_martinezcanovasalberto.util;
 
     opens es.damdi.alberto.comp_despl_p01_padressapp_martinezcanovasalberto to javafx.fxml;
     exports es.damdi.alberto.comp_despl_p01_padressapp_martinezcanovasalberto;
